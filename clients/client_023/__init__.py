77c7a38e603763541efb4b5439b8db4ca8b57af5
Z User <z@container>
2026-03-26 07:54:27 +0000

    feat(week30): Builder 1 - Client Configurations 021-025
    - Client 021: GameVerse Entertainment (Gaming, PARWA Junior)
    - Client 022: AutoDrive Motors (Automotive, PARWA High)
    - Client 023: PowerGrid Utilities (Energy, PARWA High)
    - Client 024: Daily Herald Media (Media/Publishing, PARWA Junior)
    - Client 025: ConnectTel Communications (Telecom, PARWA High)
    
    Industries: Gaming, Automotive, Energy, Media, Telecommunications
    40 tests passing
    
    CRITICAL: 5 new clients configured, all unique IDs and industries

---
+++ clients/client_023/__init__.py
@@ -0,0 +1,5 @@
+"""Client 023 - PowerGrid Utilities"""
+
+from .config import get_client_config, ClientConfig
+
+__all__ = ["get_client_config", "ClientConfig"]
---
+++ clients/client_025/__init__.py
@@ -0,0 +1,5 @@
+"""Client 025 - ConnectTel Communications"""
+
+from .config import get_client_config, ClientConfig
+
+__all__ = ["get_client_config", "ClientConfig"]
